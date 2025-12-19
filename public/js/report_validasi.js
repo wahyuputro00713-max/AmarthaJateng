bf7c5c4ed4593c55437779fb1243ee4cc8a837d6
wahyuputro00713-max <wahyuputro00713@gmail.com>
2025-12-19 18:14:59 +0700

    Update report_validasi.js

--- public/js/report_validasi.js
+++ public/js/report_validasi.js
@@ -20,7 +20,7 @@ const auth = getAuth(app);
 // ============================================================================
 // ⚠️ GANTI URL INI DENGAN URL DEPLOYMENT APPS SCRIPT ANDA (AKHIRAN /exec) ⚠️
 // ============================================================================
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzbJXZpvKdfbOiJMm3DyhKks_-VNzalVzJeCf65l8NCz4fXPrIl0NwCyknmqtaEK1_C/exec"; 
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbx-HRxqcXuXAwewr6K6zL2mMo6Ay1czK1MKWViH4-GnTJfqnexCxpgQFGoySL8LmWib/exec"; 
 
 
 // 1. Cek Login (Security)
