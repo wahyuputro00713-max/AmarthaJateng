e7918315b622a7510b41cdafe305c0739d0bb294
wahyuputro00713-max <wahyuputro00713@gmail.com>
2025-12-20 15:41:35 +0700

    Update report_validasi.js

--- public/js/report_validasi.js
+++ public/js/report_validasi.js
@@ -18,7 +18,7 @@ const auth = getAuth(app);
 const db = getDatabase(app);
 
 // ⚠️ GANTI DENGAN URL DEPLOYMENT APPS SCRIPT TERBARU ANDA ⚠️
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwAK9qA3sJqCTg4ls1EckAhnAvVesluFWcyMrsCbY1WdfOXqVpaMWlKjKbwd4Sno-jz/exec"; 
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzKSLFYjD2Z8CSW2uT59rTjGMGpaPULVKvAsHKznItHlA8WIYGOveTJEcXcPbVESStN/exec"; 
 
 // Data Dropdown Point
 const dataPoints = {
