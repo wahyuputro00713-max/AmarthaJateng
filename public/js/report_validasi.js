7d5f3ac2d4562b084a1393315570a990af3677bd
wahyuputro00713-max <wahyuputro00713@gmail.com>
2025-12-21 08:49:55 +0700

    Update report_validasi.js

--- public/js/report_validasi.js
+++ public/js/report_validasi.js
@@ -18,7 +18,7 @@ const auth = getAuth(app);
 const db = getDatabase(app);
 
 // ⚠️ PASTE URL SCRIPT BARU ANDA DISINI ⚠️
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyS_Sv3SM72NtvtwtXOTr-46H2DdGjgp_2V9ImOt6pqjuLX20Nb9FNg-AV88eomcFT1/exec"; 
+const SCRIPT_URL = "https://amarthajateng.wahyuputro00713.workers.dev"; 
 
 // Data Dropdown Point
 const dataPoints = {
