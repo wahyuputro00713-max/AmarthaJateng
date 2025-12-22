04f2256fb26fd9d75e140c7d5715bd2b82256637
wahyuputro00713-max <wahyuputro00713@gmail.com>
2025-12-22 12:51:05 +0700

    Update absensi.js nm

--- public/js/absensi.js
+++ public/js/absensi.js
@@ -66,7 +66,7 @@ const DATABASE_KOORDINAT_POINT = {
 };
 
 const MAX_JARAK_METER = 25;
-const MAX_JAM_ABSEN = "08:15";
+const MAX_JAM_ABSEN = "13:00";
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
