76763d09acc31f4931b30ab9d64fe5b55967e025
wahyuputro00713-max <wahyuputro00713@gmail.com>
2025-12-23 13:16:33 +0700

    Update report_sosialisasi.js

--- public/js/report_sosialisasi.js
+++ public/js/report_sosialisasi.js
@@ -21,7 +21,7 @@ const db = getDatabase(app);
 const SCRIPT_URL = "https://amarthajateng.wahyuputro00713.workers.dev"; 
 
 const dataPoints = {
-    "Klaten": ["01 Wedi", "Karangnongko", "Mojosongo", "Polanharjo", "Trucul"],
+    "Klaten": ["01 Wedi", "Karangnongko", "Mojosongo", "Polanharjo", "Trucuk"],
     "Magelang": ["Grabag", "Mungkid", "Pakis", "Salam"],
     "Solo": ["Banjarsari", "Gemolong", "Masaran", "Tangen"],
     "Solo 2": ["Gatak", "Jumantono", "Karanganyar", "Nguter", "Pasar kliwon"],
