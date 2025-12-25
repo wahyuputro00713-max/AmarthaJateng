1590e027b966d1a74c067636272404c7f2b4db6f
wahyuputro00713-max <wahyuputro00713@gmail.com>
2025-12-26 06:48:31 +0700

    Update absensi.js

--- public/js/absensi.js
+++ public/js/absensi.js
@@ -13,7 +13,7 @@ const firebaseConfig = {
     appId: "1:22431520744:web:711af76a5335d97179765d"
 };
 
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbz_JKscNwdqImyaqEAaHy9IN-W8C4BI4VlBYRxOipf8cZL0reOoUks6GwTxQN-176nP/exec"; 
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbySBTtCVqxKI_s74Ns7KZpv6EhQgNuek_xy5xy1e1gN9iZorRPgyMHUaHvrJzxPK-Nc/exec"; 
 
 // 1. DAFTAR POINT PER AREA (Untuk Dropdown)
 const DATA_POINTS = {
