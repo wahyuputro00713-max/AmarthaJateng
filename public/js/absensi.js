f11dc474a46fb881b332393d51d758d49ef1331e
wahyuputro00713-max <wahyuputro00713@gmail.com>
2026-01-08 08:09:02 +0700

    Update absensi.js

--- public/js/absensi.js
+++ public/js/absensi.js
@@ -12,7 +12,7 @@ const firebaseConfig = {
     appId: "1:22431520744:web:711af76a5335d97179765d"
 };
 
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwFcgXfCVZqJyAgDqhUayU0pHgGuPZjEVppUA9uJRt0mIn9y9t3wtA9uu0a82VaKEyb/exec"; 
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyF51fp-MGzZ7LH33AFuXQfMtq1aWT0n5FYw_X0wettkjen3E4nTJsdkALPwPQpZ1VO/exec"; 
 
 // --- DATA POINT & KOORDINAT ---
 const DATA_POINTS = {
