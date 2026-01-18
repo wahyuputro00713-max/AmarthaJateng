8815624a6ef98093143652fdd11592f90876659c
wahyuputro00713-max <wahyuputro00713@gmail.com>
2026-01-18 21:41:06 +0700

    Update majelis.js

--- public/js/majelis.js
+++ public/js/majelis.js
@@ -17,7 +17,7 @@ const auth = getAuth(app);
 const db = getDatabase(app);
 
 // === PASTIKAN URL INI ADALAH DEPLOYMENT TERBARU DARI CODE.GS DI ATAS ===
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzc8wtqUn_vgOiG7BRXi8JtmfgnebF0jzo1d7Ntu0mpXPjbTAu6uvXetjGh4bwFmz8D/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbykSD8oNhgA8nc6hl_ZO19q4Bzbg3k-ojWfmacglBH3vuN2NPbEAAtAyOuy2N117T3X3A/exec";
 
 // State Management
 let globalData = [];
