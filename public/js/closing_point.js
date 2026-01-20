7f52536d6e6316008672917c60b6e149bec223ca
wahyuputro00713-max <wahyuputro00713@gmail.com>
2026-01-21 05:44:26 +0700

    Update SCRIPT_URL to new Google Apps Script URL

--- public/js/closing_point.js
+++ public/js/closing_point.js
@@ -19,7 +19,7 @@ const db = getDatabase(app);
 
 // =========================================================================
 // PASTIKAN URL INI SAMA DENGAN YANG DI MAJELIS.JS
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzLrzIVSDuzTKjjEFCMfLLuz4oikSe2-Gv9ByKEdDwubRmAZULe_Wp803DU8Wb8aRmPoQ/exec"; 
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbycaapBRVnIXTZfynIGZcty6keJnQf35-RbD-r5XAWEq4tFl5xVLHrZjgYMnDUANFvmBA/exec"; 
 const ADMIN_ID = "17246";
 // =========================================================================
 
