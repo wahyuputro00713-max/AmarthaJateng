402c3fdfaf68dcd1b38f222968045b9bc8d58dd8
wahyuputro00713-max <wahyuputro00713@gmail.com>
2026-01-21 13:01:08 +0700

    Update closing_point.js

--- public/js/closing_point.js
+++ public/js/closing_point.js
@@ -19,7 +19,7 @@ const db = getDatabase(app);
 
 // =========================================================================
 // PASTIKAN URL INI SAMA DENGAN YANG DI CODE.GS
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyu_trk3LqPP4JPPoGAag3dp3w3hAFHVWSzXhF1wMCrIDf06ZFiEY3bgMQSNAXoMo4HDw/exec"; 
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyQBf5j2lBVPbr3Qv-xIZ9xTkJXLdX0jxfOtlv2MkqtWEX0mCwCkCOCe0jumQprY8qGmQ/exec"; 
 const ADMIN_ID = "17246";
 // =========================================================================
 
